2ab0506ae0b8e6320e995cd1a7e86109db054f71
uqpwalle <p.waller@uq.edu.au>
2023-06-01 08:36:05 +1000

    :tada: Initial commit

---
+++ palindromes.test.js
@@ -0,0 +1,10 @@
+// palindromes.test.js
+const palindromes = require('./palindromes')
+
+describe('palindromes()', () => {
+
+    it('correctly identifies one-word palindromes', () => {
+        expect(palindromes('madam')).toEqual(['madam'])
+    });
+
+});
